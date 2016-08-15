7b83d9276fc9c0b710fb5552ffc436fb4480c92b
Wei He <weihe924stephen@gmail.com>
2016-08-15 15:47:08 +0800

    init commit

---
+++ lib/wing/sandbox.rb
@@ -0,0 +1,7 @@
+require "wing/sandbox/version"
+
+module Wing
+  module Sandbox
+    # Your code goes here...
+  end
+end
---
+++ lib/wing/sandbox/version.rb
@@ -0,0 +1,5 @@
+module Wing
+  module Sandbox
+    VERSION = "0.1.0"
+  end
+end
